457a4f044677be21b8fc285453ae532bd164d669
fred <frederick.sonder@stit.fr>
2023-06-06 14:32:22 +0200

    suite

--- affichPdf.js
+++ affichPdf.js
@@ -1,9 +1,10 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@3.7.107/build/pdf.worker.min.js"; // spécification de l'URL du script du worker PDF.js à utiliser.
 
 const canvas = document.getElementById('pdfCanvas');                                                       // Récupération de l'élément pdfCcanvas de la modale
-const pdfPath = 'new_document.pdf';                                                                                 // Chemin vers le fichier PDF
+const pdfPath = 'https://drive.google.com/file/d/1w9DBf2frev7XWiMuqySMsyphnI6YYrie/view?usp=sharing';                                                                                 // Chemin vers le fichier PDF
+// const pdfPath = 'new_document.pdf';
 
-pdfjsLib.getDocument(pdfPath).promise.then(pdf => {                                                                 // Module de chargement du fichier PDF
+pdfjsLib.getDocument(pdfPath).promise.then(pdf => {
     return pdf.getPage(1);                                                                                          // Récupération de la première page du PDF
 }).then(page => {
     const scale = 1;                                                                                                // Définission de l'échelle pour le rendu de la page
